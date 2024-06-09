82a25817b4c095f588c25bef8f4bc827744ebaab
Tolunay Akkoyun <akko_tolu@hotmail.com>
2024-06-09 23:37:05 +0300

    Create modbus_crc.cpp

---
+++ src/modbus_crc.cpp
@@ -0,0 +1,17 @@
+#include "modbus_crc.h"
+
+uint16_t ModbusCRC::calculateCRC(uint8_t *buffer, uint16_t length) {
+    uint16_t crc = 0xFFFF;
+    for (uint16_t pos = 0; pos < length; pos++) {
+        crc ^= (uint16_t)buffer[pos];
+        for (int i = 8; i != 0; i--) {
+            if ((crc & 0x0001) != 0) {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    return crc;
+}
